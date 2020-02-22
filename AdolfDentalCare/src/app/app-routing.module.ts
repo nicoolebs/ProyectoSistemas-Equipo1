884d6881cdadf8164ba95f224e41c9c50b3b94f7
Gisellemfc <Gisellemfc@users.noreply.github.com>
2020-02-22 19:06:17 -0400

    Carpetas

--- AdolfDentalCare/src/app/app-routing.module.ts
+++ AdolfDentalCare/src/app/app-routing.module.ts
@@ -1,8 +1,14 @@
+import { InicioComponent } from './home/inicio/inicio.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { SobreNosotrosComponent } from './home/sobre-nosotros/sobre-nosotros.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'inicio', component: InicioComponent},
+  {path: 'sobre-nosotros', component: SobreNosotrosComponent},
+  {path: '', component: InicioComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- AdolfDentalCare/src/app/app.module.ts
+++ AdolfDentalCare/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './shared/header/header.component';
+import { FooterComponent } from './shared/footer/footer.component';
+import { InicioComponent } from './home/inicio/inicio.component';
+import { SobreNosotrosComponent } from './home/sobre-nosotros/sobre-nosotros.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    InicioComponent,
+    SobreNosotrosComponent,
   ],
   imports: [
     BrowserModule,
