e69d7775155a1dbd66130ecc2f4bf9620c547bc8
Gisellemfc <Gisellemfc@users.noreply.github.com>
2020-02-23 01:11:29 -0400

    Corrección dinamico
    Hacer dinamico el header

--- AdolfDentalCare/src/app/shared/header/header.component.ts
+++ AdolfDentalCare/src/app/shared/header/header.component.ts
@@ -7,6 +7,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent {
 
+  navbar: string[] = [
+    'INICIO', 'SOBRE NOSOTROS', 'SERVICIOS', 'NUESTRO PORTAL', 'CONTACTO'
+  ];
+  redesSociales: string[] = [
+    '../../../assets/mail.png', '../../../assets/instagram-2.png', '../../../assets/twitter.png', '../../../assets/whatsapp.png'
+  ];
+
   constructor() { }
 
 }
