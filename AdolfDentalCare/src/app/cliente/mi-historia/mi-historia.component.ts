3f5544864e5f2b8a3fea68e4d7250faddf941414
Gisellemfc <Gisellemfc@users.noreply.github.com>
2020-02-27 09:39:34 -0400

    Arreglo botones

--- AdolfDentalCare/src/app/cliente/mi-historia/mi-historia.component.ts
+++ AdolfDentalCare/src/app/cliente/mi-historia/mi-historia.component.ts
@@ -1,4 +1,8 @@
+import { Router } from '@angular/router';
+import { FirestoreService } from './../../services/firestore.service';
 import { Component, OnInit } from '@angular/core';
+import { AutentificacionService } from '../../services/autentificacion.service';
+import { AngularFirestore } from '@angular/fire/firestore';
 
 @Component({
   selector: 'app-mi-historia',
@@ -7,9 +11,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MiHistoriaComponent implements OnInit {
 
-  constructor() { }
+  usuario: firebase.User;
+
+  constructor(
+    private autentificacion: AutentificacionService,
+    private baseDatos: AngularFirestore,
+    private firestore: FirestoreService,
+    private router: Router
+  ) { }
 
   ngOnInit(): void {
+    this.autentificacion.getEstadoUsuario()
+    .subscribe( usuario => {
+      if(usuario){
+        this.usuario = usuario;
+      }
+    });
   }
 
 }
