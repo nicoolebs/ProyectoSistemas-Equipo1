bec26690c8db40823db43061c5da9fe23ec8662f
nicoolebs <59893575+nicoolebs@users.noreply.github.com>
2020-02-27 04:00:10 -0400

    api paypal REVISAR
    no carga completa plataforma y hay error en el TS

--- AdolfDentalCare/src/app/cliente/mis-pagos/mis-pagos.component.ts
+++ AdolfDentalCare/src/app/cliente/mis-pagos/mis-pagos.component.ts
@@ -1,11 +1,63 @@
 import { Component, OnInit } from '@angular/core';
+import { AfterViewChecked} from '@angular/core';
+
+declare let paypal: any;
 
 @Component({
   selector: 'app-mis-pagos',
   templateUrl: './mis-pagos.component.html',
   styleUrls: ['./mis-pagos.component.css']
 })
-export class MisPagosComponent implements OnInit {
+export class MisPagosComponent implements OnInit, AfterViewChecked{
+
+   
+  addScript: boolean = false;
+  paypalLoad: boolean = true;
+  
+  finalAmount: number = 1;
+ 
+  paypalConfig = {
+    env: 'sandbox',
+    client: {
+      sandbox: 'Acfn54kFJGzkmDRZAkDiRnSOnRtc05xLcuo1-3wShC8ri_zg60PPRX3yO1oM4TN6PhSWeEBq2ymNIn_v',
+      production: '<your-production-key here>'
+    },
+    commit: true,
+    payment: (data, actions) => {
+      return actions.payment.create({
+        payment: {
+          transactions: [
+            { amount: { total: this.finalAmount, currency: 'UDS' } }
+          ]
+        }
+      });
+    },
+    onAuthorize: (data, actions) => {
+      return actions.payment.execute().then((payment) => {
+        //Do something when payment is successful.
+      })
+    }
+  };
+ 
+  ngAfterViewChecked(): void {
+    if (!this.addScript) {
+      this.addPaypalScript().then(() => {
+        paypal.Button.render(this.paypalConfig, '#paypal-checkout-btn');
+        this.paypalLoad = false;
+      })
+    }
+  }
+  
+  addPaypalScript() {
+    this.addScript = true;
+    return new Promise((resolve, reject) => {
+      let scripttagElement = document.createElement('script');    
+      scripttagElement.src = 'https://www.paypalobjects.com/api/checkout.js';
+      scripttagElement.onload = resolve;
+      document.body.appendChild(scripttagElement);
+    })
+  }
+ 
 
   constructor() { }
 
