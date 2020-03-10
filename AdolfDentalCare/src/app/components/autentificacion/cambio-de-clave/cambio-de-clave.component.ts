0d2f599070177c24809ed15b58060bd5477ce173
Gisellemfc <Gisellemfc@users.noreply.github.com>
2020-03-10 16:35:05 -0400

    Ruta y componente cambio de clave

---
+++ AdolfDentalCare/src/app/components/autentificacion/cambio-de-clave/cambio-de-clave.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-cambio-de-clave',
+  templateUrl: './cambio-de-clave.component.html',
+  styleUrls: ['./cambio-de-clave.component.css']
+})
+export class CambioDeClaveComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
