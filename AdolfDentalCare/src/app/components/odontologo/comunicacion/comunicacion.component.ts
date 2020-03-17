720807911c6db5228378cd560c0cb3e7f22ad342
Gisellemfc <Gisellemfc@users.noreply.github.com>
2020-03-16 20:25:56 -0400

    Ver lista de pacientes

--- AdolfDentalCare/src/app/components/odontologo/comunicacion/comunicacion.component.ts
+++ AdolfDentalCare/src/app/components/odontologo/comunicacion/comunicacion.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FirestoreService } from '../../../services/firestore.service';
+import { AutentificacionService } from '../../../services/autentificacion.service';
 
 @Component({
   selector: 'app-comunicacion',
@@ -7,9 +9,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ComunicacionComponent implements OnInit {
 
-  constructor() { }
+  pacientes: any[] = [];
+  perfilActivo: string;
+  paciente: any = null;
+  porcentaje = '';
+  nPorcentaje: number;
+
+  constructor(private auth: AutentificacionService, private baseDatos: FirestoreService) {
+  }
 
   ngOnInit(): void {
+    // tslint:disable-next-line: prefer-for-of
+    for (let index = 0; index < this.auth.usuarioLogg.doctor.pacientes.length; index++) {
+      this.baseDatos.getDocumento(this.auth.usuarioLogg.doctor.pacientes[index], 'Usuarios').subscribe(user => {
+        this.pacientes.push(user.data());
+      });
+    }
+
+  }
+
+  actualizar() {
+
+  console.log(this.pacientes);
+
+  }
+
+  cambiar() {
   }
 
 }
