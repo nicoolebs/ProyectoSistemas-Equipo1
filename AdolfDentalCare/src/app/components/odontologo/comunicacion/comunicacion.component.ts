f1112bff1662469887f91cf9a47f4f082f188af2
Gisellemfc <Gisellemfc@users.noreply.github.com>
2020-03-20 19:37:14 -0400

    Diseño base mensaje

--- AdolfDentalCare/src/app/components/odontologo/comunicacion/comunicacion.component.ts
+++ AdolfDentalCare/src/app/components/odontologo/comunicacion/comunicacion.component.ts
@@ -9,16 +9,21 @@ import { AutentificacionService } from '../../../services/autentificacion.servic
 })
 export class ComunicacionComponent implements OnInit {
 
+  // Arreglo de pacientes
   pacientes: any[] = [];
+
   perfilActivo: string;
+  mensajeActivo: string;
+
+  tipoDeMensaje = 0;
+
   paciente: any = null;
-  porcentaje = '';
-  nPorcentaje: number;
 
   constructor(private auth: AutentificacionService, private baseDatos: FirestoreService) {
   }
 
   ngOnInit(): void {
+
     // tslint:disable-next-line: prefer-for-of
     for (let index = 0; index < this.auth.usuarioLogg.doctor.pacientes.length; index++) {
       this.baseDatos.getDocumento(this.auth.usuarioLogg.doctor.pacientes[index], 'Usuarios').subscribe(user => {
@@ -28,13 +33,22 @@ export class ComunicacionComponent implements OnInit {
 
   }
 
-  actualizar() {
+  cambiarMensaje() {
+    console.log(this.perfilActivo);
+    console.log(this.mensajeActivo);
 
-  console.log(this.pacientes);
+    if (this.mensajeActivo == 'Recordatorio de Cita') {
+      this.tipoDeMensaje = 1;
+    } else if (this.mensajeActivo == 'Recordatorio de Pago Pendiente') {
+      this.tipoDeMensaje = 2;
+    } else if (this.mensajeActivo == 'Mensaje Personalizado') {
+      this.tipoDeMensaje = 3;
+    } else {
+      this.tipoDeMensaje = 0;
+    }
 
-  }
+    console.log(this.tipoDeMensaje);
 
-  cambiar() {
   }
 
 }
