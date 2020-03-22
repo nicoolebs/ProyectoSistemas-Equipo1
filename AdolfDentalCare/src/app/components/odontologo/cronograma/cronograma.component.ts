ca429c673e0b0ddbec4c3ecb4d4d5fcb1d1ce523
Gisellemfc <Gisellemfc@users.noreply.github.com>
2020-03-22 02:02:05 -0400

    Esquema visual del cronograma

--- AdolfDentalCare/src/app/components/odontologo/cronograma/cronograma.component.ts
+++ AdolfDentalCare/src/app/components/odontologo/cronograma/cronograma.component.ts
@@ -7,6 +7,72 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CronogramaComponent implements OnInit {
 
+  cronograma = [
+    {dia: 0, horas: [
+      {hora: '10:00', libre: true},
+      {hora: '11:00', libre: true},
+      {hora: '12:00', libre: true},
+      {hora: '13:00', libre: true},
+      {hora: '14:00', libre: true},
+      {hora: '15:00', libre: true},
+      {hora: '16:00', libre: true},
+    ]},
+    {dia: 1, horas: [
+      {hora: '10:00', libre: true},
+      {hora: '11:00', libre: true},
+      {hora: '12:00', libre: true},
+      {hora: '13:00', libre: true},
+      {hora: '14:00', libre: true},
+      {hora: '15:00', libre: true},
+      {hora: '16:00', libre: true},
+    ]},
+    {dia: 2, horas: [
+      {hora: '10:00', libre: true},
+      {hora: '11:00', libre: true},
+      {hora: '12:00', libre: true},
+      {hora: '13:00', libre: true},
+      {hora: '14:00', libre: true},
+      {hora: '15:00', libre: true},
+      {hora: '16:00', libre: true},
+    ]},
+    {dia: 3, horas: [
+      {hora: '10:00', libre: true},
+      {hora: '11:00', libre: true},
+      {hora: '12:00', libre: true},
+      {hora: '13:00', libre: true},
+      {hora: '14:00', libre: true},
+      {hora: '15:00', libre: true},
+      {hora: '16:00', libre: true},
+    ]},
+    {dia: 4, horas: [
+      {hora: '10:00', libre: true},
+      {hora: '11:00', libre: true},
+      {hora: '12:00', libre: true},
+      {hora: '13:00', libre: true},
+      {hora: '14:00', libre: true},
+      {hora: '15:00', libre: true},
+      {hora: '16:00', libre: true},
+    ]},
+    {dia: 5, horas: [
+      {hora: '10:00', libre: true},
+      {hora: '11:00', libre: true},
+      {hora: '12:00', libre: true},
+      {hora: '13:00', libre: true},
+      {hora: '14:00', libre: true},
+      {hora: '15:00', libre: true},
+      {hora: '16:00', libre: true},
+    ]},
+    {dia: 6, horas: [
+      {hora: '10:00', libre: true},
+      {hora: '11:00', libre: true},
+      {hora: '12:00', libre: true},
+      {hora: '13:00', libre: true},
+      {hora: '14:00', libre: true},
+      {hora: '15:00', libre: true},
+      {hora: '16:00', libre: true},
+    ]}
+  ];
+
   constructor() { }
 
   ngOnInit(): void {
