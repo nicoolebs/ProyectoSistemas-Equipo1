bae9fd0068fd6f815b3a135e6f9480d61c309d1f
Gisellemfc <Gisellemfc@users.noreply.github.com>
2020-03-23 00:53:15 -0400

    Estructura odontologo y fecha

--- AdolfDentalCare/src/app/components/cliente/mis-citas/crear-cita/crear-cita.component.ts
+++ AdolfDentalCare/src/app/components/cliente/mis-citas/crear-cita/crear-cita.component.ts
@@ -7,9 +7,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CrearCitaComponent implements OnInit {
 
+  doctorActivo: string;
+  doctores: any[];
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  actualizar() {
+
   }
 
 }
