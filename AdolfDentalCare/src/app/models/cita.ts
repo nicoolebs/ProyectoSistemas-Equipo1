c257c33ea7249139546211584d7a3a02f7e8d814
Gisellemfc <Gisellemfc@users.noreply.github.com>
2020-03-22 22:00:38 -0400

    Estructura cita

--- AdolfDentalCare/src/app/models/cita.ts
+++ AdolfDentalCare/src/app/models/cita.ts
@@ -1,2 +1,10 @@
 export interface Cita {
+  doctor: string;
+  paciente: string;
+  fecha: string;
+  tratamiento: string;
+  confirmada: boolean;
+  costo?: number;
+  recipe?: string;
+  id?: string;
 }
