ce563c2936c9397943148172157594cf7f8c5081
Leonardo Gonzalez <59893051+Leopgf@users.noreply.github.com>
2020-03-25 00:41:37 -0400

    Modificar Paciente lsito

--- AdolfDentalCare/src/app/components/cliente/mi-perfil/modificar-perfil/modificar-perfil.component.ts
+++ AdolfDentalCare/src/app/components/cliente/mi-perfil/modificar-perfil/modificar-perfil.component.ts
@@ -11,6 +11,11 @@ export class ModificarPerfilComponent implements OnInit {
 
     usuario;
     nombre;
+    apellido;
+    direccion;
+    fecha;
+    sexo;
+    telefono;
   constructor(
     private firestore: FirestoreService,
     private authentication : AutentificacionService
@@ -22,9 +27,30 @@ export class ModificarPerfilComponent implements OnInit {
   ngOnInit(): void {
   }
   
-    guardarNombre(){
-      console.log(this.usuario.uid),
-      console.log(this.nombre),
-      this.firestore.updateCat(this.usuario.uid,this.nombre,'Usuarios')
+    guardarApellido(){
+      this.usuario.paciente.apellido = this.apellido;
+      this.firestore.updateCat(this.usuario.uid,this.usuario,'Usuarios')
     }
+  
+    guardarDireccion(){
+      this.usuario.paciente.direccion = this.direccion;
+      this.firestore.updateCat(this.usuario.uid,this.usuario,'Usuarios')
+    }
+    guardarFecha(){
+      this.usuario.paciente.nacimiento = this.fecha;
+      this.firestore.updateCat(this.usuario.uid,this.usuario,'Usuarios')
+    }
+    guardarSexo(){
+      this.usuario.paciente.genero = this.sexo;
+      this.firestore.updateCat(this.usuario.uid,this.usuario,'Usuarios')
+    }
+    guardarTelefono(){
+      this.usuario.paciente.telefono = this.telefono;
+      this.firestore.updateCat(this.usuario.uid,this.usuario,'Usuarios')
+    }
+    guardarNombre(){
+      this.usuario.paciente.nombre = this.nombre;
+      this.firestore.updateCat(this.usuario.uid,this.usuario,'Usuarios')
+    }
+
 }
