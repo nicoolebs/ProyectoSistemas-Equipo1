fb99dd6c6a40c3d78f793d24c6ddaa86176d7443
Gisellemfc <Gisellemfc@users.noreply.github.com>
2020-03-25 12:12:03 -0400

    Update administrar-historias.component.ts

--- AdolfDentalCare/src/app/components/odontologo/administrar-historias/administrar-historias.component.ts
+++ AdolfDentalCare/src/app/components/odontologo/administrar-historias/administrar-historias.component.ts
@@ -1,6 +1,7 @@
 import { AutentificacionService } from './../../../services/autentificacion.service';
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder, Form } from '@angular/forms';
+import { FirestoreService } from '../../../services/firestore.service';
 
 @Component({
   selector: 'app-administrar-historias',
@@ -10,10 +11,18 @@ import { FormGroup, FormBuilder, Form } from '@angular/forms';
 export class AdministrarHistoriasComponent implements OnInit {
 
   creacionPaciente = false;
+  pacientes: any[] = [];
 
-  constructor(private auth: AutentificacionService) { }
+  constructor(private auth: AutentificacionService, private baseDatos: FirestoreService) { }
 
   ngOnInit(): void {
+    for (let index = 0; index < this.auth.usuarioLogg.doctor.pacientes.length; index++) {
+
+      this.baseDatos.getDocumento(this.auth.usuarioLogg.doctor.pacientes[index],'Usuarios').subscribe(paciente => {
+        this.pacientes.push(paciente.data);
+      });
+
+    }
   }
 
   verForm() {
