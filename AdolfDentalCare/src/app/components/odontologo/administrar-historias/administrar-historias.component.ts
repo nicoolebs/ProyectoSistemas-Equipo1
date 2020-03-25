2ca0f480d973a1cfc46e19d1af640aeb86c05eff
Gisellemfc <Gisellemfc@users.noreply.github.com>
2020-03-25 13:22:50 -0400

    Ver historia odontologo

--- AdolfDentalCare/src/app/components/odontologo/administrar-historias/administrar-historias.component.ts
+++ AdolfDentalCare/src/app/components/odontologo/administrar-historias/administrar-historias.component.ts
@@ -12,21 +12,47 @@ export class AdministrarHistoriasComponent implements OnInit {
 
   creacionPaciente = false;
   pacientes: any[] = [];
+  pacienteActivo: any;
+  paciente: any;
+  seleccionado = false;
+  historia: any[] = [];
 
   constructor(private auth: AutentificacionService, private baseDatos: FirestoreService) { }
 
   ngOnInit(): void {
     for (let index = 0; index < this.auth.usuarioLogg.doctor.pacientes.length; index++) {
 
-      this.baseDatos.getDocumento(this.auth.usuarioLogg.doctor.pacientes[index],'Usuarios').subscribe(paciente => {
-        this.pacientes.push(paciente.data);
+      this.baseDatos.getDocumento(this.auth.usuarioLogg.doctor.pacientes[index], 'Usuarios').subscribe(paciente => {
+        this.pacientes.push(paciente.data());
+        console.log(this.pacientes);
       });
 
     }
+
   }
 
   verForm() {
+
     this.creacionPaciente = !this.creacionPaciente;
+
+  }
+
+  verHistoria() {
+
+    this.paciente = this.pacientes.filter(filtro => filtro.uid === this.pacienteActivo.substring(0, 28));
+    this.paciente = this.paciente[0];
+
+    for (let index = 0; index < this.paciente.paciente.historia.length; index++) {
+      this.baseDatos.getDocumento(this.paciente.paciente.historia[index],'Citas').subscribe(cita => {
+        this.historia.push(cita.data());
+      });
+    }
+
+    console.log(this.historia);
+
+
+    this.seleccionado = true;
+
 
   }
 }
