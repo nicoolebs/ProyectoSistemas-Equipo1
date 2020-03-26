9dd572d389bc5e53a44f17809193c3688e91e84a
Gisellemfc <Gisellemfc@users.noreply.github.com>
2020-03-26 12:26:11 -0400

    Reportar pago zelle y banco

--- AdolfDentalCare/src/app/components/cliente/pagar/pagar.component.ts
+++ AdolfDentalCare/src/app/components/cliente/pagar/pagar.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit, AfterViewChecked } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { AutentificacionService } from '../../../services/autentificacion.service';
 import { FirestoreService } from '../../../services/firestore.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-pagar',
@@ -60,13 +61,21 @@ export class PagarComponent implements OnInit {
   pagosPendientes: any[] = [];
   citaSeleccionada: boolean[] = [];
   citasPagar: any[] = [];
+  monto: number = 0;
   pago = false;
   metodoDePago: any;
+
+  email: any;
+  nombre: any;
+  referencia: any;
+  titular: any;
+  banco: any;
 
 
   constructor(
     private auth: AutentificacionService,
-    private baseDatos: FirestoreService
+    private baseDatos: FirestoreService,
+    private router: Router
   ) { }
 
   ngOnInit(): void {
@@ -87,16 +96,74 @@ export class PagarComponent implements OnInit {
   seleccionarCitas() {
 
     this.citasPagar = [];
+    this.monto = 0;
 
     for (let index = 0; index < this.citaSeleccionada.length; index++) {
 
       if (this.citaSeleccionada[index] === true) {
         this.citasPagar.push(this.pagosPendientes[index]);
+        this.monto = this.monto + this.pagosPendientes[index].costo;
       }
 
     }
 
     this.pago = true;
+
+  }
+
+  reportarPagoZelle() {
+    let factura = {
+      info:
+      'Email: ' + this.email + ', Nombre: ' + this.nombre + ', Referencia: ' + this.referencia,
+      paciente: this.auth.usuarioLogg.uid,
+      monto: this.monto,
+      ref: this.referencia,
+      fecha: new Date(),
+      medio: 'Zelle'
+    };
+
+    this.baseDatos.createDocumento(factura, 'Facturas', factura.paciente + factura.fecha.toString()).then( corr => {
+
+      for (let index = 0; index < this.citasPagar.length; index++) {
+
+        this.citasPagar[index].paga = true;
+
+        this.baseDatos.updateDocumento(this.citasPagar[index].id, this.citasPagar[index], 'Citas');
+
+      }
+
+      console.log('Pago reportado con éxito.');
+
+      this.router.navigate(['dashboard-paciente/mis-pagos']);
+    });
+
+  }
+
+  reportarPagoBanco() {
+    let factura = {
+      info:
+      'Titular: ' + this.titular + ', Banco: ' + this.banco + ', Referencia: ' + this.referencia,
+      paciente: this.auth.usuarioLogg.uid,
+      monto: this.monto,
+      ref: this.referencia,
+      fecha: new Date(),
+      medio: 'Banco'
+    };
+
+    this.baseDatos.createDocumento(factura, 'Facturas', factura.paciente + factura.fecha.toString()).then( corr => {
+
+      for (let index = 0; index < this.citasPagar.length; index++) {
+
+        this.citasPagar[index].paga = true;
+
+        this.baseDatos.updateDocumento(this.citasPagar[index].id, this.citasPagar[index], 'Citas');
+
+      }
+
+      console.log('Pago reportado con éxito.');
+
+      this.router.navigate(['dashboard-paciente/mis-pagos']);
+    });
 
   }
 }
