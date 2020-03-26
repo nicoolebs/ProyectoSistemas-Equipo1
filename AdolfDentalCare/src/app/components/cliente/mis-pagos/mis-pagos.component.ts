7c8380e841104e5dc6de9722d06fce62521eacbe
Gisellemfc <Gisellemfc@users.noreply.github.com>
2020-03-26 10:46:43 -0400

    ver fact pendientes paciente

--- AdolfDentalCare/src/app/components/cliente/mis-pagos/mis-pagos.component.ts
+++ AdolfDentalCare/src/app/components/cliente/mis-pagos/mis-pagos.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit} from '@angular/core';
+import { AutentificacionService } from '../../../services/autentificacion.service';
+import { FirestoreService } from '../../../services/firestore.service';
 
 declare let paypal: any;
 
@@ -9,10 +11,27 @@ declare let paypal: any;
 })
 export class MisPagosComponent implements OnInit{
 
+  deuda: number;
+  pagosPendientes: any[] = [];
 
-  constructor() { }
+
+  constructor(
+    private auth: AutentificacionService,
+    private baseDatos: FirestoreService) { }
 
   ngOnInit(): void {
+    for (let index = 0; index < this.auth.usuarioLogg.paciente.historia.length; index++) {
+
+      this.baseDatos.getDocumento(this.auth.usuarioLogg.paciente.historia[index], 'Citas').subscribe(cita => {
+
+        if (cita.data().paga === false && cita.data().costo !== 0) {
+          this.pagosPendientes.push(cita.data());
+        }
+
+      });
+
+    }
+
   }
 
 }
