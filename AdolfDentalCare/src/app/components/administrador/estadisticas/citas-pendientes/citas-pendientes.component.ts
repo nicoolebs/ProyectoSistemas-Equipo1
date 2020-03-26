169d75349032477a198706b0ca0fca172100e917
Gisellemfc <Gisellemfc@users.noreply.github.com>
2020-03-26 19:09:29 -0400

    Ingresos por fecha

--- AdolfDentalCare/src/app/components/administrador/estadisticas/citas-pendientes/citas-pendientes.component.ts
+++ AdolfDentalCare/src/app/components/administrador/estadisticas/citas-pendientes/citas-pendientes.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FirestoreService } from '../../../../services/firestore.service';
 
 @Component({
   selector: 'app-citas-pendientes',
@@ -7,9 +8,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CitasPendientesComponent implements OnInit {
 
-  constructor() { }
+  citasPendientes: any[] = [];
+
+  constructor(private baseDatos: FirestoreService) { }
 
   ngOnInit(): void {
+
+    this.citasPendientes = [];
+
+    this.baseDatos.getDocumentos('Citas').subscribe(citas => {
+
+      let agendaCitas = []
+
+      for (let index = 0; index < citas.length; index++) {
+
+        agendaCitas.push(citas[index].payload.doc.data());
+
+      }
+
+      for (let index = 0; index < agendaCitas.length; index++) {
+
+        if (agendaCitas[index].costo === 0) {
+
+          this.citasPendientes.push(agendaCitas[index]);
+        }
+
+      }
+    })
   }
 
 }
--- AdolfDentalCare/src/app/components/administrador/estadisticas/ingresos/ingresos.component.ts
+++ AdolfDentalCare/src/app/components/administrador/estadisticas/ingresos/ingresos.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FirestoreService } from '../../../../services/firestore.service';
 
 @Component({
   selector: 'app-ingresos',
@@ -7,9 +8,60 @@ import { Component, OnInit } from '@angular/core';
 })
 export class IngresosComponent implements OnInit {
 
-  constructor() { }
+  fechaInicial: Date;
+  fechaFinal: Date;
+  ver = false;
+
+  cantidad: number;
+  agendaCitas: any[] = [];
+
+  constructor(private baseDatos: FirestoreService) { }
 
   ngOnInit(): void {
+    this.cantidad = 0;
+
+    this.baseDatos.getDocumentos('Facturas').subscribe(citas => {
+
+      this.agendaCitas = [];
+
+      for (let index = 0; index < citas.length; index++) {
+
+        this.agendaCitas.push(citas[index].payload.doc.data());
+
+      }
+    });
+  }
+
+  verEstadistica() {
+
+    this.cantidad = 0;
+    this.fechaFinal = new Date(this.fechaFinal);
+    this.fechaInicial = new Date(this.fechaInicial);
+
+    if (this.fechaFinal != undefined && this.fechaInicial != undefined) {
+
+      for (let index = 0; index < this.agendaCitas.length; index++) {
+
+        let fecha = new Date(this.agendaCitas[index].fecha);
+
+        console.log(fecha);
+
+
+        if (fecha.getTime() <= this.fechaFinal.getTime() && fecha.getTime() >= this.fechaInicial.getTime()) {
+
+          this.cantidad = this.cantidad + this.agendaCitas[index].monto;
+        }
+
+      }
+
+      this.ver = true;
+
+    } else {
+
+      alert('Ingrese un rango de fechas válido.');
+      this.ver = false;
+    }
+
   }
 
 }
