cfbaa76ebe53ede5cca0876ce3af5adfa1d1fcce
Gisellemfc <Gisellemfc@users.noreply.github.com>
2020-03-27 02:07:50 -0400

    configuracion actual

--- AdolfDentalCare/src/app/components/odontologo/medios-pago/medios-pago.component.ts
+++ AdolfDentalCare/src/app/components/odontologo/medios-pago/medios-pago.component.ts
@@ -11,13 +11,15 @@ export class MediosPagoComponent implements OnInit {
   doctor = this.auth.usuarioLogg;
   banco = this.doctor.doctor.mediosPago.banco;
   zelle = this.doctor.doctor.mediosPago.zelle;
-  
+  paypal = this.doctor.doctor.mediosPago.paypal;
+
   constructor(
     private firebase : FirestoreService,
     private auth : AutentificacionService
   ) { }
 
   ngOnInit(): void {
+
   }
 
 }
